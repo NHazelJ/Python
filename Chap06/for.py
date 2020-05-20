e4a16b90ca0f144e8e267d304738f1fcfe1fb6ab
NHazelJ <najib.mosquera@hotmail.com>
2020-05-20 00:17:06 -0400

    practicing loops, breaks, continue, else

--- Chap06/for.py
+++ Chap06/for.py
@@ -4,5 +4,8 @@
 animals = ( 'lion', 'bunny', 'dog', 'cat', 'velociraptor' )
 
 for pet in animals:
+    #if pet == "dog": continue
+    #if pet == "velociraptor": break
     print(pet)
-
+else:
+    print("that is all of the animals")
